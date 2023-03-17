2605a904852b478f7b344bb95dd16b49d25170d7
wooYeon-gitHub <dndus9510@gmail.com>
2023-03-17 17:38:04 +0900

    tictactoe 게임 프로젝트 생성

---
+++ src/components/Board.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react'
+import Square from './Square'
+import "./Board.css"
+
+export default class Board extends Component {
+
+  renderSqiare(i) {
+    return <Square />
+  }
+
+
+  render() {
+    return (
+      <div>
+        <div className='status'>Next Player: X, 0</div>
+        <div className='board-row'>
+          {this.renderSqiare(0)}
+          {this.renderSqiare(1)}
+          {this.renderSqiare(2)}
+        </div>
+        <div className='board-row'>
+          {this.renderSqiare(3)}
+          {this.renderSqiare(4)}
+          {this.renderSqiare(5)}
+        </div>
+        <div className='board-row'>
+          {this.renderSqiare(6)}
+          {this.renderSqiare(7)}
+          {this.renderSqiare(8)}
+        </div>
+      </div>
+    )
+  }
+}
